3996bf8c69897d31a6d5abaaaa067646cf4cc43d
Stanimir <stanimirkosev00@gmail.com>
2022-04-02 20:02:41 +0300

    added array and printing on the display

--- script.js
+++ script.js
@@ -32,64 +32,72 @@ function operate(operator,a,b){
 
 }
 
+let displayValue = [];
+
 const calc = document.getElementById('calc');
 
     const seven = document.createElement('button');
     seven.textContent = '7';
     calc.appendChild(seven).className = 'div1';
     seven.addEventListener("click",function(){
-        text.textContent= '7';
+        displayValue.push('7');
+        reduceTry();
     })
-
 
     const eight = document.createElement('button');
     eight.textContent = '8';
     calc.appendChild(eight).className = 'div1';
     eight.addEventListener("click",function(){
-        text.textContent= '8';
+        displayValue.push('8');
+        reduceTry();
     })
-
 
     const nine = document.createElement('button');
     nine.textContent = '9';
     calc.appendChild(nine).className = 'div1';
     nine.addEventListener("click",function(){
-        text.textContent= '9';
+        displayValue.push('9');
+        reduceTry();
     })
 
     const divide = document.createElement('button');
     divide.textContent = '/';
     calc.appendChild(divide).className = 'operators';
     divide.addEventListener("click",function(){
-        text.textContent= '/';
+        displayValue.push('/');
+        reduceTry();
     })
     
     const four = document.createElement('button');
     four.textContent = '4';
     calc.appendChild(four).className = 'div1';
     four.addEventListener("click",function(){
-        text.textContent= '4';
+        displayValue.push('4');
+        reduceTry();
     })
     
     const five = document.createElement('button');
     five.textContent = '5';
     calc.appendChild(five).className = 'div1';
     five.addEventListener("click",function(){
-        text.textContent= '5';
+        displayValue.push('5');
+        reduceTry();
     })
     
     const six = document.createElement('button');
     six.textContent = '6';
     calc.appendChild(six).className = 'div1';
     six.addEventListener("click",function(){
-        text.textContent= '6';
+        displayValue.push('6');
+        reduceTry();
     })
     
     const asteriks = document.createElement('button');
     asteriks.textContent = '*';
     calc.appendChild(asteriks).className = 'operators';
     asteriks.addEventListener("click",function(){
-        text.textContent= '*';
+        displayValue.push('*');
+        reduceTry();
     })
 
 
@@ -97,14 +105,16 @@ const calc = document.getElementById('calc');
     three.textContent = '3';
     calc.appendChild(three).className = 'div1';
     three.addEventListener("click",function(){
-        text.textContent= '3';
+        displayValue.push('3');
+        reduceTry();
     })
     
     const two = document.createElement('button');
     two.textContent = '2';
     calc.appendChild(two).className = 'div1';
     two.addEventListener("click",function(){
-        text.textContent= '2';
+        displayValue.push('2');
+        reduceTry();
     })
 
 
@@ -112,42 +122,48 @@ const calc = document.getElementById('calc');
     one.textContent = '1';
     calc.appendChild(one).className = 'div1';
     one.addEventListener("click",function(){
-        text.textContent= '1';
+        displayValue.push('1');
+        reduceTry();
     })
     
     const minus = document.createElement('button');
     minus.textContent = '-';
     calc.appendChild(minus).className = 'operators';
     minus.addEventListener("click",function(){
-        text.textContent= '-';
+        displayValue.push('-');
+        reduceTry();
     })
 
     const zero = document.createElement('button');
     zero.textContent = '0';
     calc.appendChild(zero).className = 'div1';
     zero.addEventListener("click",function(){
-        text.textContent= '0';
+        displayValue.push('0');
+        reduceTry();
     })
 
     const decimal = document.createElement('button');
     decimal.textContent = '.';
     calc.appendChild(decimal).className = 'div1';
     decimal.addEventListener("click",function(){
-        text.textContent= '.';
+        displayValue.push('.');
+        reduceTry();
     })
 
     const equals = document.createElement('button');
     equals.textContent = '=';
     calc.appendChild(equals).className = 'equals div1';
     equals.addEventListener("click",function(){
-        text.textContent= '=';
+        /*text.textContent= '=';
+        displayValue.push('='); */
     })
     
     const plus = document.createElement('button');
     plus.textContent = '+';
     calc.appendChild(plus).className = 'operators';
     plus.addEventListener("click",function(){
-        text.textContent= '+';
+        displayValue.push('+');
+        reduceTry();
     })
 
 const txtNclear = document.getElementById('txtNclear');
@@ -159,6 +175,10 @@ const txtNclear = document.getElementById('txtNclear');
     clear.textContent = 'AC';
     txtNclear.appendChild(clear).className = 'allClear operators';    
 
-    
 
-   
+function reduceTry(){
+    
+    const pls = displayValue.reduce((total,value) => {
+       return text.innerText = total + value;
+    },'');
+};
