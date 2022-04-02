0d2de47e62ca9e2b7345038229edc65f42dc83f0
Stanimir <stanimirkosev00@gmail.com>
2022-04-02 10:21:23 +0300

    JS calculator functions d

--- script.js
+++ script.js
@@ -8,8 +8,28 @@ function subtract(a,b){
 
 function multiply(a,b){
     return a*b;
-}
+};
 
 function divison(a,b){
     return a/b;
 };
+
+function operate(operator,a,b){
+    
+    switch (operator){
+        case '+':    
+            return add(a,b);
+        
+        case '-':
+            return subtract(a,b);
+           
+        case '*':
+            return multiply(a,b);
+        
+        case '/':
+            return divison(a,b);    
+    }
+
+}
+
+console.log(operate('+',5,5));
